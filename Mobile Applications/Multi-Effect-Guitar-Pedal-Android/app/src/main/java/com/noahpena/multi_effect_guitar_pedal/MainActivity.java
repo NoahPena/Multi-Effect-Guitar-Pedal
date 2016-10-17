a5a58bab7563dd6be41f89e3027044f11b104c91
Noah Pena <noahpena@Noahs-MacBook-Pro.local>
2016-10-17 15:58:44 -0500

    Created Android and iOS Projects

---
+++ Mobile Applications/Multi-Effect-Guitar-Pedal-Android/app/src/main/java/com/noahpena/multi_effect_guitar_pedal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.noahpena.multi_effect_guitar_pedal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
