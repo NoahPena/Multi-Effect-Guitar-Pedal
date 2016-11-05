f45955538e820933f88931f3290094b828e43ff5
Noah Pena <noahpena@Noahs-MacBook-Pro.local>
2016-11-05 14:31:26 -0500

    Go NP_Dota

--- Mobile Applications/Multi-Effect-Guitar-Pedal-Android/app/src/main/java/com/noahpena/multi_effect_guitar_pedal/Adapters/MainMenuAdapter.java
+++ Mobile Applications/Multi-Effect-Guitar-Pedal-Android/app/src/main/java/com/noahpena/multi_effect_guitar_pedal/Adapters/MainMenuAdapter.java
@@ -1,10 +1,15 @@
 package com.noahpena.multi_effect_guitar_pedal.Adapters;
 
+import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.noahpena.multi_effect_guitar_pedal.R;
 
@@ -15,30 +20,95 @@ import com.noahpena.multi_effect_guitar_pedal.R;
 public class MainMenuAdapter extends BaseAdapter
 {
 
-//    private Drawable[] itemOne = {R.drawable.graphic_equalizer};
+    private String itemOneText = "Create/Edit Effects";
+    private String itemTwoText = "Create/Play Set List";
 
-    public MainMenuAdapter()
+    private int[] itemOne = {R.drawable.graphic_equalizer, R.drawable.graphic_equalizer_glow};
+    private int[] itemTwo = {R.drawable.electric_guitar_outline, R.drawable.electric_guitar_outline_glow};
+
+    private Activity mActivity;
+    private LayoutInflater inflater;
+
+    public MainMenuAdapter(Activity activity)
     {
-
+        this.mActivity = activity;
     }
 
     @Override
-    public int getCount() {
-        return 0;
+    public int getCount()
+    {
+        return 2;
     }
 
     @Override
-    public Object getItem(int i) {
+    public Object getItem(int i)
+    {
         return null;
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int i)
+    {
+        return i;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(final int i, View view, ViewGroup viewGroup)
+    {
+        inflater = mActivity.getLayoutInflater();
+
+        View row = inflater.inflate(R.layout.mainmenu_item, null);
+
+        final ImageView imageView = (ImageView) row.findViewById(R.id.mainMenuItemImage);
+        TextView textView = (TextView) row.findViewById(R.id.mainMenuItemText);
+
+        if(i == 0)
+        {
+            imageView.setImageResource(itemOne[0]);
+            textView.setText(itemOneText);
+        }
+        else
+        {
+            imageView.setImageResource(itemTwo[0]);
+            textView.setText(itemTwoText);
+        }
+
+        row.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    if(i == 0)
+                    {
+                        imageView.setImageResource(itemOne[1]);
+                    }
+                    else
+                    {
+                        imageView.setImageResource(itemTwo[1]);
+                    }
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    if(i == 0)
+                    {
+                        imageView.setImageResource(itemOne[0]);
+                    }
+                    else
+                    {
+                        imageView.setImageResource(itemTwo[0]);
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+        return row;
+
+
     }
 }
